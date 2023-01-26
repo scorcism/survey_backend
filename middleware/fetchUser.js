7a7d587d46622eed64c5f99dd58f8cab52b474a0
scorcism <abhishekpathak1720@gmail.com>
2023-01-27 01:24:41 +0530

    fetch user from the jwt token -middleware

---
+++ middleware/fetchUser.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const fetchuser = (req, res, next) => {
+    const token = req.header("auth-token");
+    if (!token) {
+        res.status(401).json({ error: "Authenticate using valid token" });
+    }
+    try {
+        const data = jwt.verify(token.process.env.JWT_SECRET);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).json({ error: "Authenticate using valid token" });
+    }
+}
+
+
+module.exports = fetchuser;
