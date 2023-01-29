32d5a11a0a4a8037c9f53d942a7e5b679122becc
scorcism <abhishekpathak1720@gmail.com>
2023-01-29 20:44:21 +0530

    refs

---
+++ url_refs.js
@@ -0,0 +1,27 @@
+const url_refs =
+    [
+        {
+            "Backend": {
+                "Node Js": "https://nodejs.org/",
+                "Express Js": "https://expressjs.com/",
+                "Nodemon": "https://www.npmjs.com/package/nodemon",
+                "Mongo DB": "https://www.mongodb.com/",
+                "Mongoose": "https://mongoosejs.com/",
+                "express_validator": "https://express-validator.github.io/docs/",
+                "bcryptjs": "https://www.npmjs.com/package/bcryptjs",
+                "jwt token": "https://www.npmjs.com/package/jsonwebtoken",
+                "dotenv": "https://www.npmjs.com/package/dotenv",
+            },
+        }, {
+            "Frontend": {
+                "React JS": {
+                    url: "https://reactjs.org/",
+                    purpose: ""
+                },
+            },
+        }
+    ]
+
+
+
+
